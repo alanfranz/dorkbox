b6a19942794769704395231209c68d362aa31c64
Alan Franzoni <username@franzoni.eu>
2016-03-12 19:10:40 +0100

    Remove obsolete distributions; add wily

--- build-images/ubuntu-vivid/test/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-dpkg -i /build/*.deb || /bin/true
-apt-get -y -f install
-git config --global user.email "john@example.com"
-git config --global user.name "John Doe"
-/opt/dorkbox/bin/test --force
